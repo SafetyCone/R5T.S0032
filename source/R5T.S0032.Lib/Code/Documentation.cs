8a0053bddea7af5f1b7cccb67751dca45b89822b
davidcoats <david@getsafetycone.com>
2022-05-07 12:41:13 -0700

    Initial commit of source-stub.

---
+++ source/R5T.S0032.Lib/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.S0032.Lib
+{
+    /// <summary>
+    /// Context-less code syntax element creation.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
